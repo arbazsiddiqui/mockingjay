6cb4a7d95597e0855ee55237423eb0a99d4237ad
arbazsiddiqui <arbaz00@gmail.com>
2018-04-02 01:29:07 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const Chance = require("chance");
+
+const chance = new Chance();
+const supportedChanceType = Object.keys(Object.getPrototypeOf(chance));
+
+const checkValidity = (type) => {
+	return supportedChanceType.includes(type)
+};
+
+const getValue = (type) => {
+	if (checkValidity(type))
+		return chance[type]();
+	else
+		return null;
+};
+
+const generateArray = (length, type) => {
+	return new Array(length).fill(null).map((_, i) => {
+		return getValue(type);
+	});
+};
+
+const main = (type) => {
+	if (Array.isArray(type)) {
+		const chanceType = type[0];
+		const numberOfData = type[1];
+		return generateArray(numberOfData, chanceType);
+	}
+	return getValue(type);
+};
+
+module.exports = {
+	main
+};
